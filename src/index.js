deb4f540406dd6e019e21f2ad8edb90281b32d95
bismark <bismark.john.anes@gmail.com>
2023-02-12 12:17:46 +0700

    Restyle and only use dimension attributes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import styled from 'styled-components';
 
 export const ContainerRowFlex = styled.div`
+  width: ${props => props.width};
+  height: ${props => props.height || 'initial'};
+
   display: flex;
   justify-content: ${props => props.justify || 'space-between' };
   align-items: ${props => props.align || 'center'};
-  width: ${props => props.width};
-  height: ${props => props.height || 'initial'};
-  margin-top:${props => props.top};
-  margin-bottom: ${props => props.bottom ? props.bottom : 0};
-  padding-left: ${props => props.paddingHorizontal ? props.paddingHorizontal : 0};
-  padding-right: ${props => props.paddingHorizontal ? props.paddingHorizontal : 0};
   flex: ${props => props.flex || 'none'};
-  background: ${props => props.background || 'initial'};
-  background-color: ${props => props.backgroundColor || 'initial'};
   flex-wrap: ${props => props.wrap || 'initial'};
+
+  margin: ${props => props.margin || 0};
+  margin-top:${props => props.marginTop};
+  margin-bottom: ${props => props.marginBottom};
+  margin-left: ${props => props.marginLeft};
+  margin-right: ${props => props.marginRight};
+
+  padding: ${props => props.padding || 0};
+  padding-top: ${props => props.paddingTop};
+  padding-bottom: ${props => props.paddingBottom};
+  padding-left: ${props => props.paddingLeft};
+  padding-right: ${props => props.paddingRight};
+
+  border: ${props => props.border || 'initial'}
 `;
