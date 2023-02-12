86ad8717ede9bf6a201f932726ad3d62183554d9
bismark <bismark.john.anes@gmail.com>
2023-02-12 12:17:46 +0700

    Restyle and only use dimension attributes

--- example/src/App.js
+++ example/src/App.js
@@ -5,9 +5,49 @@ import { ContainerRowFlex } from 'container-base'
 class Example extends React.Component {
   render() {
     return (
-      <ContainerRowFlex backgroundColor="red" >
-        Hello
-      </ContainerRowFlex>
+      <React.Fragment>
+        <ContainerRowFlex
+          justify="space-around"
+          height="100vh"
+        >
+          <ContainerRowFlex
+            border="2px solid grey"
+            justify="center"
+            height="250px"
+            width="250px"
+          >
+            <p>Nunc rutrum turpis sed pede</p>
+          </ContainerRowFlex>
+
+          <ContainerRowFlex
+            border="2px solid grey"
+            justify="center"
+            height="250px"
+            width="250px"
+          >
+            <p>Lorem ipsum dolor sit amet</p>
+          </ContainerRowFlex>
+
+          <ContainerRowFlex
+            border="2px solid grey"
+            justify="flex-end"
+            align="flex-end"
+            height="250px"
+            width="250px"
+          >
+            <p>Nullam tristique diam non turpis.</p>
+          </ContainerRowFlex>
+
+          <ContainerRowFlex
+            border="2px solid grey"
+            padding="16px"
+            height="250px"
+            width="250px"
+          >
+            <p>Donec neque quam, dignissim in</p>
+          </ContainerRowFlex>
+        </ContainerRowFlex>
+      </React.Fragment>
     )
   }
 }
