2f4d26371db1b232b39bda46b7c8f78530b2f343
bismark <bismark.john.anes@gmail.com>
2023-02-14 12:38:54 +0700

    Add container base block

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export const ContainerBase = styled.div`
-  width: ${props => props.width};
-  height: ${props => props.height || 'initial'};
+const _container = styled.div`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height || 'initial'};
+  position: ${(props) => props.position};
+  display: ${(props) => props.display};
 
+  margin: ${(props) => props.margin || 0};
+  margin-top: ${(props) => props.marginTop};
+  margin-bottom: ${(props) => props.marginBottom};
+  margin-left: ${(props) => props.marginLeft};
+  margin-right: ${(props) => props.marginRight};
+
+  padding: ${(props) => props.padding || 0};
+  padding-top: ${(props) => props.paddingTop};
+  padding-bottom: ${(props) => props.paddingBottom};
+  padding-left: ${(props) => props.paddingLeft};
+  padding-right: ${(props) => props.paddingRight};
+
+  border: ${(props) => props.border || 'initial'};
+`
+export const ContainerBaseRow = styled(_container)`
   display: flex;
-  justify-content: ${props => props.justify || 'space-between' };
-  align-items: ${props => props.align || 'center'};
-  flex: ${props => props.flex || 'none'};
-  flex-wrap: ${props => props.wrap || 'initial'};
+  justify-content: ${(props) => props.justify || 'space-between'};
+  align-items: ${(props) => props.align || 'center'};
+  flex: ${(props) => props.flex || 'none'};
+  flex-wrap: ${(props) => props.wrap || 'initial'};
+`
 
-  margin: ${props => props.margin || 0};
-  margin-top:${props => props.marginTop};
-  margin-bottom: ${props => props.marginBottom};
-  margin-left: ${props => props.marginLeft};
-  margin-right: ${props => props.marginRight};
+export const ContainerBaseBlock = styled(_container)`
+  display: block;
+`
 
-  padding: ${props => props.padding || 0};
-  padding-top: ${props => props.paddingTop};
-  padding-bottom: ${props => props.paddingBottom};
-  padding-left: ${props => props.paddingLeft};
-  padding-right: ${props => props.paddingRight};
-
-  border: ${props => props.border || 'initial'}
-`;
+// alias
+export const ContainerBase = ContainerBaseRow
