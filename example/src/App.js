6aea72c68574538776ca8f082d175755baba346f
bismark <bismark.john.anes@gmail.com>
2023-02-14 12:38:54 +0700

    Add container base block

--- example/src/App.js
+++ example/src/App.js
@@ -1,34 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
+import { ContainerBaseRow, ContainerBaseBlock } from 'container-base'
 
-import { ContainerBase } from 'container-base'
+
+const DivBackground = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: purple;
+`;
 
 class Example extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <ContainerBase
+
+        <ContainerBaseRow
           justify="space-around"
-          height="100vh"
         >
-          <ContainerBase
+          <ContainerBaseRow
             border="2px solid grey"
             justify="center"
             height="250px"
             width="250px"
           >
-            <p>Nunc rutrum turpis sed pede</p>
-          </ContainerBase>
+            <ContainerBaseBlock>
+              <p>1. Nunc rutrum turpis sed pede</p>
+              <p>2. Lorem ipsum dolor sit amet</p>
+            </ContainerBaseBlock>
+          </ContainerBaseRow>
 
-          <ContainerBase
+          <ContainerBaseRow
             border="2px solid grey"
             justify="center"
             height="250px"
             width="250px"
           >
             <p>Lorem ipsum dolor sit amet</p>
-          </ContainerBase>
+          </ContainerBaseRow>
 
-          <ContainerBase
+          <ContainerBaseRow
             border="2px solid grey"
             justify="flex-end"
             align="flex-end"
@@ -36,17 +49,77 @@ class Example extends React.Component {
             width="250px"
           >
             <p>Nullam tristique diam non turpis.</p>
-          </ContainerBase>
+          </ContainerBaseRow>
 
-          <ContainerBase
+          <ContainerBaseRow
             border="2px solid grey"
             padding="16px"
             height="250px"
             width="250px"
           >
             <p>Donec neque quam, dignissim in</p>
-          </ContainerBase>
-        </ContainerBase>
+          </ContainerBaseRow>
+        </ContainerBaseRow>
+
+
+
+        <ContainerBaseRow>
+          <ContainerBaseBlock>
+            <ContainerBaseRow
+              border="2px solid grey"
+              justify="space-between"
+              align="center"
+              height="250px"
+              width="500px"
+            >
+              <p>Row 1, Block 1, Row 1</p>
+              <p>Row 1, Block 1, Row 2</p>
+            </ContainerBaseRow>
+            <ContainerBaseRow
+              border="2px solid grey"
+              justify="space-around"
+              align="flex-start"
+              height="250px"
+              width="500px"
+            >
+              <p>Row 1, Block 2, Row 1</p>
+              <p>Row 1, Block 2, Row 2</p>
+            </ContainerBaseRow>
+          </ContainerBaseBlock>
+
+          <ContainerBaseBlock>
+            <ContainerBaseRow
+              border="2px solid grey"
+              justify="space-between"
+              align="center"
+              height="250px"
+              width="500px"
+            >
+              <p>Row 2, Block 1, Row 1</p>
+              <p>Row 2, Block 1, Row 2</p>
+            </ContainerBaseRow>
+            <ContainerBaseRow
+              border="2px solid grey"
+              justify="space-around"
+              align="flex-start"
+              height="250px"
+              width="500px"
+            >
+              <p>Row 2, Block 2, Row 1</p>
+              <p>Row 2, Block 2, Row 2</p>
+            </ContainerBaseRow>
+          </ContainerBaseBlock>
+        </ContainerBaseRow>
+
+        <ContainerBaseRow
+          width="250px"
+          height="250px"
+          border="4px solid red"
+          position="relative"
+        >
+          <DivBackground/>
+        </ContainerBaseRow>
+
       </React.Fragment>
     )
   }
